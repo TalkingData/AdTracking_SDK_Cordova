dbfe21daaa48c3ec723a4ea60e848f16bac6a64c
xiaofei <xiaofei.liu@tendcloud.com>
2019-10-11 10:00:15 +0800

    新增android 桥阶层接口
    Change-Id: Ia43e70f69f2d94659fde2fe8ca47515bb4597ba8

--- src/android/TalkingDataAppCpaPlugin.java
+++ src/android/TalkingDataAppCpaPlugin.java
@@ -1,9 +1,11 @@
 package com.talkingdata.adtracking;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 import com.tendcloud.appcpa.Order;
 import com.tendcloud.appcpa.ShoppingCart;
+import com.tendcloud.appcpa.TDTransaction;
 import com.tendcloud.appcpa.TalkingDataAppCpa;
 
 import org.apache.cordova.CallbackContext;
@@ -16,9 +18,6 @@ import org.json.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 
 public class TalkingDataAppCpaPlugin extends CordovaPlugin {
@@ -29,7 +28,7 @@ public class TalkingDataAppCpaPlugin extends CordovaPlugin {
         super.initialize(cordova, webView);
         this.ctx = cordova.getActivity().getApplicationContext();
     }
-    
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if ("init".equals(action)) {
@@ -127,16 +126,121 @@ public class TalkingDataAppCpaPlugin extends CordovaPlugin {
             } catch (InvocationTargetException e) {
                 e.printStackTrace();
             }
+        } else if (action.startsWith("onTransaction")) {
+            String accountId = args.getString(0);
+            String transaction = args.getString(1);
+            TalkingDataAppCpa.onTransaction(accountId, stringToTransaction(transaction));
+            return true;
+        } else if (action.startsWith("onCredit")) {
+            String accountId = args.getString(0);
+            int amount = args.getInt(1);
+            String content = args.getString(2);
+            TalkingDataAppCpa.onCredit(accountId, amount, content);
+            return true;
+        } else if (action.startsWith("onFavorite")) {
+            String category = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onFavorite(category, content);
+            return true;
+        } else if (action.startsWith("onShare")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onShare(accountId, content);
+            return true;
+        } else if (action.startsWith("onPunch")) {
+            String accountId = args.getString(0);
+            String punchId  = args.getString(1);
+            TalkingDataAppCpa.onPunch(accountId, punchId);
+            return true;
+        } else if (action.startsWith("onReservation")) {
+            String accountId = args.getString(0);
+            String reservationId = args.getString(1);
+            String category = args.getString(2);
+            int amount = args.getInt(3);
+            String term = args.getString(4);
+            TalkingDataAppCpa.onReservation(accountId, reservationId, category, amount, term);
+            return true;
+        } else if (action.startsWith("onBooking")) {
+            String accountId = args.getString(0);
+            String bookingId = args.getString(1);
+            String category = args.getString(2);
+            int amount = args.getInt(3);
+            String content = args.getString(4);
+            TalkingDataAppCpa.onBooking(accountId, bookingId, category, amount, content);
+            return true;
+        } else if (action.startsWith("onContact")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onContact(accountId, content);
+            return true;
+        } else if (action.startsWith("onLearn")) {
+            String accountId = args.getString(0);
+            String course = args.getString(1);
+            long begin = args.getLong(2);
+            int duration = args.getInt(3);
+            TalkingDataAppCpa.onLearn(accountId, course, begin, duration);
+            return true;
+        } else if (action.startsWith("onRead")) {
+            String accountId = args.getString(0);
+            String book = args.getString(1);
+            long begin = args.getLong(2);
+            int duration = args.getInt(3);
+            TalkingDataAppCpa.onRead(accountId, book, begin, duration);
+            return true;
+        } else if (action.startsWith("onBrowse")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            long begin = args.getLong(2);
+            int duration = args.getInt(3);
+            TalkingDataAppCpa.onBrowse(accountId, content, begin, duration);
+            return true;
+        } else if (action.startsWith("onChargeBack")) {
+            String accountId = args.getString(0);
+            String orderId = args.getString(1);
+            String reason = args.getString(2);
+            String type = args.getString(3);
+            TalkingDataAppCpa.onChargeBack(accountId, orderId, reason, type);
+            return true;
+        } else if (action.startsWith("onTrialFinished")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onTrialFinished(accountId, content);
+            return true;
+        } else if (action.startsWith("onGuideFinished")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onGuideFinished(accountId, content);
+            return true;
+        }else if (action.startsWith("onPreviewFinished")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onPreviewFinished(accountId, content);
+            return true;
+        }else if (action.startsWith("onFreeFinished")) {
+            String accountId = args.getString(0);
+            String content = args.getString(1);
+            TalkingDataAppCpa.onFreeFinished(accountId, content);
+            return true;
+        }else if (action.startsWith("onLevelPass")) {
+            String accountId = args.getString(0);
+            String levelId = args.getString(1);
+            TalkingDataAppCpa.onLevelPass(accountId, levelId);
+            return true;
+        }else if (action.startsWith("onAchievementUnlock")) {
+            String accountId = args.getString(0);
+            String achievementId = args.getString(1);
+            TalkingDataAppCpa.onAchievementUnlock(accountId, achievementId);
+            return true;
         }
         return false;
     }
-    
+
     private Order stringToOrder(String orderStr) {
         try {
             JSONObject orderJson = new JSONObject(orderStr);
             Order order = Order.createOrder(orderJson.getString("orderId"), orderJson.getInt("total"), orderJson.getString("currencyType"));
             JSONArray items = orderJson.getJSONArray("items");
-            for (int i=0; i<items.length(); i++) {
+            for (int i = 0; i < items.length(); i++) {
                 JSONObject item = items.getJSONObject(i);
                 order.addItem(item.getString("itemId"), item.getString("category"), item.getString("name"), item.getInt("unitPrice"), item.getInt("amount"));
             }
@@ -146,13 +250,13 @@ public class TalkingDataAppCpaPlugin extends CordovaPlugin {
         }
         return null;
     }
-    
+
     private ShoppingCart stringToShoppingCart(String shoppingCartStr) {
         try {
             JSONObject shoppingCartJson = new JSONObject(shoppingCartStr);
             ShoppingCart shoppingCart = ShoppingCart.createShoppingCart();
             JSONArray items = shoppingCartJson.getJSONArray("items");
-            for (int i=0; i<items.length(); i++) {
+            for (int i = 0; i < items.length(); i++) {
                 JSONObject item = items.getJSONObject(i);
                 shoppingCart.addItem(item.getString("itemId"), item.getString("category"), item.getString("name"), item.getInt("unitPrice"), item.getInt("amount"));
             }
@@ -161,5 +265,28 @@ public class TalkingDataAppCpaPlugin extends CordovaPlugin {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private TDTransaction stringToTransaction(String json) {
+        TDTransaction tdTransaction = TDTransaction.createTDTransaction();
+        if (TextUtils.isEmpty(json)) {
+            return tdTransaction;
+        }
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            tdTransaction.setTransactionId(jsonObject.optString("transactionId", null))
+                    .setCategory(jsonObject.optString("category", null))
+                    .setAmount(jsonObject.optInt("amount", 0))
+                    .setPersonA(jsonObject.optString("personA", null))
+                    .setPersonB(jsonObject.optString("personB", null))
+                    .setStartDate(jsonObject.optLong("startDate", 0))
+                    .setEndDate(jsonObject.optLong("endDate", 0))
+                    .setContent(jsonObject.optString("content", null))
+                    .setCurrencyType(jsonObject.optString("currencyType", null));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tdTransaction;
     }
 }
